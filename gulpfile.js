5aba3214d10805865bcba99d1e7e96b0797c5b1a
artpros94 <prostakishyn.artem@icloud.com>
2020-08-08 17:34:34 +0300

    added header

--- gulpfile.js
+++ gulpfile.js
@@ -2,27 +2,27 @@ let gulp = require('gulp'),
     sass = require('gulp-sass'),
     rename = require('gulp-rename'),
     browserSync = require('browser-sync'),
-    autoprefixer = require('gulp-autoprefixer')
+    autoprefixer = require('gulp-autoprefixer'),
     concat = require('gulp-concat'),
     uglify = require('gulp-uglify'),
     cssmin = require('gulp-cssmin');
 
 gulp.task('sass', function(){
-    gulp.src('app/scss/style.scss')
-     .pipe(sass({outputStyle: 'compressed'}))
-     .pipe(rename({suffix : '.min'}))
-     .pipe(autoprefixer({
-        overrideBrowserslist: ['last 8 version']
-     }))
-     .pipe(gulp.dest('app/css'))
-     .pipe(browserSync.reload({stream: true}))
+  return  gulp.src('app/scss/**/*.scss')
+         .pipe(sass({outputStyle: 'compressed'}))
+         .pipe(rename({suffix : '.min'}))
+         .pipe(autoprefixer({
+            overrideBrowserslist: ['last 8 version']
+         }))
+         .pipe(gulp.dest('app/css'))
+         .pipe(browserSync.reload({stream: true}))
 });
 
 gulp.task('style', function(){
-    return gulp.src([
-        'node_modules/normalize.css/normalize.css',
-        'node_modules/slick-carousel/slick/slick.css',
-        'node_modules/magnific-popup/dist/magnific-popup.css'
+  return gulp.src([
+    'node_modules/normalize.css/normalize.css',
+    'node_modules/slick-carousel/slick/slick.css',
+    'node_modules/magnific-popup/dist/magnific-popup.css'
     ])
           .pipe(concat('libs.min.css'))
           .pipe(cssmin())
@@ -58,7 +58,7 @@ gulp.task('browser-sync', function() {
 });
 
 gulp.task('watch', function(){
-    gulp.watch('app/scss/style.scss', gulp.parallel('sass'));
+    gulp.watch('app/scss/**/*.scss', gulp.parallel('sass'));
     gulp.watch('app/*.html', gulp.parallel('html'));
     gulp.watch('app/js/*.js', gulp.parallel('js'));
 });
